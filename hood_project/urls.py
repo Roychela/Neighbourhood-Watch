73390629f479ba34246fe75c475e1483e27ce614
Roy Chela <roychela@gmail.com>
2019-08-11 14:58:23 +0300

    Specify logout template

--- hood_project/urls.py
+++ hood_project/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', include('hood.urls')),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout')
 ]
