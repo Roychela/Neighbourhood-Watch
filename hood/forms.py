9bce693ab8c6bf572736eb253ba29e3ab42d5b4d
Roy Chela <roychela@gmail.com>
2019-08-12 02:53:14 +0300

    Add BusinessForm, PostForm forms as well as modify ProfileUpdateForm to include the name field

--- hood/forms.py
+++ hood/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile, Neighbourhood
+from .models import Profile, Neighbourhood, Business, Post
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image','bio']
+        fields = ['name','image','bio']
 
 
 class UserRegisterForm(UserCreationForm):
@@ -25,3 +25,13 @@ class CreateHoodForm(forms.ModelForm):
      class Meta:
         model = Neighbourhood
         fields = ('__all__')
+class BusinessForm(forms.ModelForm):
+     class Meta:
+        model = Business
+        exclude = ('business_owner','neighbourhood')
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        exclude = ('user', 'neighbourhood')
