ababe344ef0cb663c3246779d7d8f9f3e790b28a
Roy Chela <roychela@gmail.com>
2019-08-11 16:14:05 +0300

    Add Profile model with automatic user profile creation and saving

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
+    bio = models.TextField(default='default')
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
