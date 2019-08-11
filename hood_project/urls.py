f0ef88b675902343c07040c0581dad1ba340c794
Roy Chela <roychela@gmail.com>
2019-08-11 13:32:07 +0300

    Add user registration path

--- hood_project/urls.py
+++ hood_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from hood import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hood.urls')),
+    path('register/', views.register, name='register'),
 ]
