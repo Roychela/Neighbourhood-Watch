69ca0835a8632869b2e6b715c1f1e17cdd68aaf7
Roy Chela <roychela@gmail.com>
2019-08-11 16:17:13 +0300

    Add profile url path

--- hood_project/urls.py
+++ hood_project/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hood.urls')),
+    path('profile/', views.profile, name='profile'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout')
