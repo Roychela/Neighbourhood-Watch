f1428ea17fc1644a6f51aa3d4bf78d86a12cfebb
Roy Chela <roychela@gmail.com>
2019-08-11 16:20:21 +0300

    Add if statement for static url patterns

--- hood/urls.py
+++ hood/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
      path('', views.home, name='home'),
 ]
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
