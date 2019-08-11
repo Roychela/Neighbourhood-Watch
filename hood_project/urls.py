a982d4433db6456f19df9444eb557d984ae79c65
Roy Chela <roychela@gmail.com>
2019-08-11 13:55:52 +0300

    Add login and logout url paths

--- hood_project/urls.py
+++ hood_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hood import views 
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hood.urls')),
     path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
