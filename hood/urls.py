597e5310ee6a829e9cfc45b90fb42abb15ae3dd4
Roy Chela <roychela@gmail.com>
2019-08-12 02:51:14 +0300

    Add url paths for create_business, user_neighbourhood and create_post view functions

--- hood/urls.py
+++ hood/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
      path('new/neighbourhood/', views.create_neighbourhood, name='createhood'),
      path('join/(?P<neighbourhood_id>\d+)/', views.join_neighbourhood, name='join'),
      path('leave/(?P<neighbourhood_id>\d+)/', views.leave_neighbourhood, name='leave'),
+     path('new/business/', views.create_business, name='createbusiness'),
+     path('userhood/', views.user_neighbourhood, name='userneighbourhoods'),
+     path('new/post/', views.create_post, name='createpost'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
