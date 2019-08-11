5acab2b7cf2791fc94bee7cac51a3fb2dd2ee299
Roy Chela <roychela@gmail.com>
2019-08-11 23:00:33 +0300

    Add leave and join neighbourhood url paths

--- hood/urls.py
+++ hood/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
      path('', views.home, name='home'),
-     path('new/neighbourhood/', views.create_neighbourhood, name='createhood')
+     path('new/neighbourhood/', views.create_neighbourhood, name='createhood'),
+     path('join/(?P<neighbourhood_id>\d+)/', views.join_neighbourhood, name='join'),
+     path('leave/(?P<neighbourhood_id>\d+)/', views.leave_neighbourhood, name='leave'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
